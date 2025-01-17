699c214e483e8112709b438db9696816ce0d4b0b
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-01-17 12:22:32 -0600

    interfaz ver.1

---
+++ interfazLicuadora.java
@@ -0,0 +1,8 @@
+public interface interfazLicuadora {
+    public void apagar();
+    public void aumentarVel();
+    public void llenar();
+    public void vaciar();
+    public void checkLlenar();
+    public void checkVelocidad();
+}
